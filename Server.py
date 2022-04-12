d5b29f2322f638334c4a31c5244a5990ffd7a3cd
Shuo Wang <89996485+YUCHen9967@users.noreply.github.com>
2022-04-12 14:48:10 +0800

    Update Server.py

--- Server.py
+++ Server.py
@@ -54,21 +54,179 @@ def home():  # put application's code here
 @app.route('/login', methods=('GET', 'POST'))
 def login():
     if request.method == 'POST':
-        username = request.form['email']
-        query = "SELECT * FROM Users AS U WHERE U.email = '{}'".format(username)
+        email = request.form['email']
+        print(email)
+        query = "SELECT * FROM Users AS U WHERE U.email = '{}'".format(email)
         user = g.conn.execute(query).fetchone()
-
         if user is None:
-            query = "SELECT * FROM admin AS a WHERE a.email = '{}'".format(username)
+            query = "SELECT * FROM admin AS a WHERE a.email = '{0}'".format(email)
             admin = g.conn.execute(query).fetchone()
             if admin is None:
                 return redirect('/wrong')
             else:
-                return redirect('/')
+                return render_template('user.html', **context)
         else:
-            return redirect('/')
-    return render_template("login.html")
+            query1="WITH tmp1(userid) AS (SELECT userid FROM Users WHERE email='{0}') SELECT M.Name FROM MOVIES AS M,(SELECT MC.mid FROM Movie_collection AS MC, tmp1 AS U WHERE U.userid=MC.userid) AS tmp WHERE M.mid=tmp.mid".format(email)
+            cursor = g.conn.execute(query1)
+            movies = []
+            for result in cursor:
+                print(result)
+                movies.append(result[0])  # can also be accessed using result[0]
+            cursor.close()
+            print(movies)
+            context = dict(data=movies)
+            return render_template('user.html', **context)
 
+    return render_template("login.html")
+@app.route('/add_collection',methods=('GET', 'POST'))
+def add_collection():
+    if request.method == 'POST':
+        email = request.form['email']
+        movietitle=request.form['movie']
+        error=None
+
+        query1 = "SELECT COUNT(*) FROM Users WHERE email = '{0}'".format(email)
+        query2 = "SELECT COUNT(*) FROM MOVIES WHERE Name = '{0}'".format(movietitle)
+        usercheck=g.conn.execute(query1).fetchone()
+        moviecheck=g.conn.execute(query2).fetchone()
+        if usercheck is None:
+            error = 'User is already registered.'
+            return render_template('wrong.html')
+        elif moviecheck is None:
+            error = 'No such movie.'
+            return render_template('wronguser.html')
+
+        query3 = "SELECT userid FROM Users WHERE email = '{0}'".format(email)
+        query4 = "SELECT mid FROM MOVIES WHERE Name = '{0}'".format(movietitle)
+
+        mid=0
+        userid=0
+        cursor = g.conn.execute(query3)
+        for result in cursor:
+            print(result)
+            userid=result[0]
+        cursor.close()
+        cursor = g.conn.execute(query4)
+        for result in cursor:
+            mid=result[0]
+        cursor.close()
+
+
+        if error is None:
+            query = "INSERT INTO Movie_collection (userid, mid) VALUES ('{0}','{1}')".format(userid, mid)
+            g.conn.execute(query)
+            return redirect(request.referrer)
+    return render_template("add_collection.html")
+
+@app.route('/searchcele',methods=('GET', 'POST'))
+def searchcele():
+    if request.method == 'POST':
+        email = request.form['email']
+        search = request.form['search']
+        query1 = "SELECT COUNT(*) FROM Users WHERE email = '{0}'".format(email)
+        usercheck = g.conn.execute(query1).fetchone()[0]
+        if usercheck<1:
+            error = 'User is already registered.'
+            return render_template('wrong.html')
+        else:
+            query1="SELECT userid FROM Users WHERE email = '{0}'".format(email)
+            userid=0
+            cursor=g.conn.execute(query1)
+            for result in cursor:
+                userid=result[0]  # can also be accessed using result[0]
+            cursor.close()
+        query3 = "SELECT COUNT(*) FROM Celebrity WHERE name = '{0}'".format(search)
+        celebritycheck = g.conn.execute(query3).fetchone()[0]
+        if celebritycheck<1:
+            return render_template('wrongcele.html')
+        else:
+            query3 = "SELECT * FROM Celebrity WHERE name='{0}'".format(search)
+            cursor = g.conn.execute(query3)
+            Celebrity = []
+            for result in cursor:
+                Celebrity.append(result['name'])  # can also be accessed using result[0]
+            cursor.close()
+            query4 = "SELECT celeid FROM Celebrity WHERE name = '{0}'".format(search)
+            cursor = g.conn.execute(query4)
+            celeid=0
+            for result in cursor:
+                celeid=result[0]  # can also be accessed using result[0]
+            cursor.close()
+            query5 = "INSERT INTO Search_celebrity (userid, celeid) VALUES ('{0}','{1}')".format(userid, celeid)
+            g.conn.execute(query5)
+            context = dict(data=Celebrity)
+            return render_template("celebrity.html", **context)
+    return render_template("search.html")
+
+@app.route('/searchmovie',methods=('GET', 'POST'))
+def searchmovie():
+    if request.method == 'POST':
+        email = request.form['email']
+        search = request.form['search']
+        query1 = "SELECT COUNT(*) FROM Users WHERE email = '{0}'".format(email)
+        usercheck = g.conn.execute(query1).fetchone()[0]
+        if usercheck<1:
+            return render_template('wrong.html')
+        else:
+            query1="SELECT userid FROM Users WHERE email = '{0}'".format(email)
+            userid=0
+            cursor=g.conn.execute(query1)
+            for result in cursor:
+                userid=result[0]  # can also be accessed using result[0]
+            cursor.close()
+        query2 = "SELECT COUNT(*) FROM MOVIES WHERE Name = '{0}'".format(search)
+
+        moviecheck = g.conn.execute(query2).fetchone()[0]
+        print(moviecheck)
+        if moviecheck<1:
+            return render_template('wronguser.html')
+        else:
+            query5 = "SELECT * FROM MOVIES WHERE Name='{0}'".format(search)
+            cursor=g.conn.execute(query5)
+            movieTitle = ""
+            movieDetail = 0
+            movieStoryline = 0
+            moviegenre = 0
+            movieid = 0
+
+            for result in cursor:
+                movieTitle = result['name']
+                movieDetail = result["details"]
+                movieStoryline = result["storyline"]
+                moviegenre = result['genre']
+                movieid = result['mid']
+            cursor.close()
+            movierate = ""
+            query = "SELECT round(avg(UR.rate),1) AS rate FROM User_rate AS UR WHERE UR.mid='{0}'".format(movieid)
+            cursor = g.conn.execute(query)
+            for result in cursor:
+                movierate = result['rate']
+            cursor.close()
+
+            query = "SELECT C1.name FROM Celebrity AS C1, (SELECT celeid FROM Act AS A WHERE A.mid='{0}') AS C2 WHERE C1.celeid=C2.celeid".format(
+                movieid)
+            cursor = g.conn.execute(query)
+            actors = []
+            cursor = g.conn.execute(query)
+            for result in cursor:
+                actors.append(result[0])
+            cursor.close()
+            query = "SELECT DISTINCT U.username, UC.comment FROM Users AS U, User_comment AS UC WHERE UC.mid='{0}' AND U.userid=UC.userid".format(
+                movieid)
+            cursor = g.conn.execute(query)
+            Comment = []
+            Username = []
+            cursor = g.conn.execute(query)
+            for result in cursor:
+                Username.append(result[0])
+                Comment.append(result[1])
+            cursor.close()
+            query5 = "INSERT INTO Search(userid, mid) VALUES ('{0}','{1}')".format(userid, movieid)
+            g.conn.execute(query5)
+        return render_template("movie1.html", title=movieTitle, Detail=movieDetail,
+                               Storyline=movieStoryline, actors=actors,
+                               genre=moviegenre, rate=movierate, comments=Comment, Username=Username)
+    return render_template("search.html")
 
 @app.route('/wrong')
 def wrong():
@@ -179,9 +337,10 @@ def comment():
             query = "INSERT INTO User_comment (cid, userid, mid, comment) VALUES ('{0}','{1}','{2}','{3}')".format(
                 cid, userid, mid, comment)
             g.conn.execute(query)
-            return redirect('/')
+            return redirect(request.referrer)
 
     return render_template('comment.html')
+
 @app.route('/celebrity')
 def Celebrity():
     cursor = g.conn.execute("SELECT * FROM Celebrity ")
@@ -218,14 +377,6 @@ def star(name):
     cursor.close()
     return render_template("star.html", name=celebrityname, Detail=detail)
 
-@app.route('/user')
-def user():
-    cursor = g.conn.execute("SELECT * FROM MOVIES AS M ")
-    movies = []
-    for result in cursor:
-        movies.append(result['name'])  # can also be accessed using result[0]
-    cursor.close()
-    return render_template("user.html")
 
 
 @app.route('/admin')
